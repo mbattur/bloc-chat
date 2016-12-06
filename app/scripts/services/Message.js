01f0ec80347480ba7fdb7a3522a103d66b5d98ce
mbattur <mbattur@yahoo.com>
2016-12-06 19:43:32 +0000

    ongoing 2

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -3,13 +3,7 @@
         var ref = firebase.database().ref().child("messages");
         var messages = $firebaseArray(ref);
 
-        return {
-            getByRoomId: function (roomId) {
-            ref.orderByChild('roomId').equalTo('-KXl1GDd7Fv64hmY8GkO').on('value', function(snapshot) {
-                console.log(snapshot.val());
-            });
-            }
-        };
+        
     }
 
     angular
--- dist/scripts/services/Room.js
+++ dist/scripts/services/Room.js
@@ -2,12 +2,26 @@
     function Room($firebaseArray) {
         var ref = firebase.database().ref();
         var rooms = $firebaseArray(ref.child('rooms'));
+        var messages = $firebaseArray(ref.child('messages'));
         
         return {
             all: rooms,
             
             create: function(room){
                 return rooms.$add(room);
+            },
+            
+            addMessage: function(userName, content, room) {
+                messages.$add({
+                    username: userName,
+                    content: content,
+                    sent_at: firebase.database.ServerValue.TIMESTAMP,
+                    roomid: room.$id
+                });
+            },
+            
+            getMessages: function(room){
+                return $firebaseArray(messages.orderByChild('roomid').equalTo(room.$id));
             }
         };
     }
