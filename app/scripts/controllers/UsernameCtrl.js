d02583b60916980d596cae8578e6d04d9a968ab2
mbattur <mbattur@yahoo.com>
2016-12-12 18:27:01 +0000

    checkpoint-5 completed

---
+++ app/scripts/controllers/UsernameCtrl.js
@@ -0,0 +1,16 @@
+(function() {
+    function UsernameCtrl($modalInstance, $cookies) {
+        this.submit = function() {
+            if(this.text) {
+                console.log(this.text);
+                $cookies.put('blocChatCurrentUser', this.text);
+                this.text = '';
+                $modalInstance.close();
+            }
+        };
+    }
+    
+    angular
+        .module('blocChat')
+        .controller('UsernameCtrl', ['$modalInstance', '$cookies', UsernameCtrl]);
+})();
---
+++ dist/scripts/services/BlocChatCookies.js
@@ -0,0 +1,17 @@
+(function() {
+    function BlocChatCookies($cookies, $modal) {
+        var currentUser = $cookies.get('blocChatCurrentUser');
+        if (!currentUser || currentUser === '') {
+            $modal.open({
+                templateUrl: 'templates/username.html',
+                controller: 'UsernameCtrl as user',
+                backdrop: 'static',
+                keyboard: false
+            });
+        }
+    }
+    
+    angular
+        .module('blocChat')
+        .run(['$cookies', '$modal', BlocChatCookies]);
+})();
